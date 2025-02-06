b5bb7ff7cc22de7d401d6d7821eb539123f5395b
UhNess <jokes0607@yahoo.com>
2025-02-06 14:47:37 -0500

    MORE style added to ABOUT.tsx

--- stickersmash/app/(tabs)/about.tsx
+++ stickersmash/app/(tabs)/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About Us</Text>
+      <Text style={styles.title}>About Us</Text>
       <Text style={styles.text}>CSCI 3660, Mobile Application Development</Text>
       <Text style={styles.text}>Group 3 Members:</Text>
       <Text style={styles.text}>Sam Charles, 
@@ -13,7 +13,7 @@ export default function AboutScreen() {
         Clark Serecky, 
         Damien Smith
       </Text>
-      
+
       <Text style={styles.text}>To log in:</Text>
       <Text style={styles.text}>Username: admin</Text>
       <Text style={styles.text}>Password: password123</Text>
@@ -30,5 +30,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: "#fff",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 40,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
   },
 });
